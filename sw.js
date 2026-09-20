d1f48305a96d432b973c994316a9becb16408041
nigamsarma <94115584+nigamsarma@users.noreply.github.com>
2026-09-21 02:05:34 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -28,6 +28,6 @@ self.addEventListener('push', function(event) {
 self.addEventListener('notificationclick', function(event) {
   event.notification.close();
   event.waitUntil(
-    clients.openWindow('youtube.html')
+    clients.openWindow('www.youtube.com')
   );
 });
